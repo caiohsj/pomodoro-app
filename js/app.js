60c942d84a4a4d6f622b1664757a25bee684775a
Caio Henrique <caiocoxim10@gmail.com>
2020-12-02 20:41:22 -0400

    implementado temporizador + alerta simples a cada 4 pomodoros

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 var count = localStorage.getItem('count');
-var timerPomodoro = document.getElementById('timer-pomodoro').value;
-var timerInterval = document.getElementById('timer-interval').value;
+var timerPomodoro = document.getElementById('timer-pomodoro');
+var timerInterval = document.getElementById('timer-interval');
 var labelMinutes = document.querySelector('.timer .minutes');
 var labelSeconds = document.querySelector('.timer .seconds');
 var button = document.querySelector('.btn-play'); 
+var interval;
 
 if (count == null) {
     count = 0;
@@ -12,14 +13,41 @@ if (count == null) {
 
 button.addEventListener('click', function(event) {
     event.preventDefault();
-    if (count < 4) {
-        count++;
-    } else {
-        count = 0;
-    }
-    setCount(count);
+    initLabelMinutesAndLabelSeconds();
+    interval = setInterval(decreaseSecond, 1000);
+    event.target.style.display = 'none';
 });
+
+function initLabelMinutesAndLabelSeconds() {
+    labelMinutes.innerHTML = timerPomodoro.value - 1;
+    labelSeconds.innerHTML = 59;
+}
+
+function setLabelMinutesAndLabelSeconds() {
+    labelMinutes.innerHTML = labelMinutes.textContent - 1;
+    labelSeconds.innerHTML = 59;
+}
 
 function setCount(value) {
     localStorage.setItem('count', value);
+}
+
+function decreaseSecond() {
+    if (labelMinutes.textContent >= 0 && labelSeconds.textContent > 0) {
+        if (labelSeconds.textContent > 0) {
+            labelSeconds.innerHTML = labelSeconds.textContent - 1;
+        } else {
+            setLabelMinutesAndLabelSeconds();
+        }
+    } else {
+        if (count < 3) {
+            count++;
+        } else {
+            alert('ora do intervalo');
+            count = 0;
+        }
+        setCount(count);
+        button.style.display = 'block';
+        clearInterval(interval);
+    }
 }
