060147810485c311a71099fc360dc5910b14999d
Caio Henrique <caiocoxim10@gmail.com>
2020-12-03 07:30:08 -0400

    correções na lógica do timer + timer de intervalo + notificações + contagens de pomodoros

--- js/app.js
+++ js/app.js
@@ -1,53 +1,108 @@
 var count = localStorage.getItem('count');
+var amountOfPomodoros = localStorage.getItem('amountOfPomodoros');
 var timerPomodoro = document.getElementById('timer-pomodoro');
 var timerInterval = document.getElementById('timer-interval');
+var alarm = document.getElementById('sound');
 var labelMinutes = document.querySelector('.timer .minutes');
 var labelSeconds = document.querySelector('.timer .seconds');
-var button = document.querySelector('.btn-play'); 
+var labelAmountOfPomodoros = document.querySelector('.amount-pomodoros');
+var button = document.querySelector('.btn-play');
+var buttonInterval = document.querySelector('.btn-play-interval'); 
 var interval;
+var activeInterval = false;
+
+Notification.requestPermission().then(function(result) {});
 
 if (count == null) {
     count = 0;
     setCount(count);
 }
 
+if (amountOfPomodoros == null) {
+    amountOfPomodoros = 0;
+    setAmountOfPomodoros(amountOfPomodoros);
+}
+
+setLabelAmountOfPomodoros(amountOfPomodoros);
+
 button.addEventListener('click', function(event) {
     event.preventDefault();
-    initLabelMinutesAndLabelSeconds();
+    initLabelMinutesAndLabelSecondsForPomodoro();
     interval = setInterval(decreaseSecond, 1000);
     event.target.style.display = 'none';
+    buttonInterval.style.display = 'none';
 });
 
-function initLabelMinutesAndLabelSeconds() {
+buttonInterval.addEventListener('click', function(event) {
+    event.preventDefault();
+    activeInterval = true;
+    initLabelMinutesAndLabelSecondsForInterval();
+    interval = setInterval(decreaseSecond, 1000);
+    event.target.style.display = 'none';
+    button.style.display = 'none';
+});
+
+function initLabelMinutesAndLabelSecondsForPomodoro() {
     labelMinutes.innerHTML = timerPomodoro.value - 1;
-    labelSeconds.innerHTML = 59;
+    labelSeconds.innerHTML = 5;
+}
+
+function initLabelMinutesAndLabelSecondsForInterval() {
+    labelMinutes.innerHTML = timerInterval.value - 1;
+    labelSeconds.innerHTML = 5;
 }
 
 function setLabelMinutesAndLabelSeconds() {
     labelMinutes.innerHTML = labelMinutes.textContent - 1;
-    labelSeconds.innerHTML = 59;
+    labelSeconds.innerHTML = 5;
+}
+
+function setLabelAmountOfPomodoros(value) {
+    labelAmountOfPomodoros.innerHTML = '('+value+')';
 }
 
 function setCount(value) {
     localStorage.setItem('count', value);
 }
 
+function setAmountOfPomodoros(value) {
+    localStorage.setItem('amountOfPomodoros', value);
+}
+
+function showButtons() {
+    buttonInterval.style.display = 'inline';
+    button.style.display = 'inline';
+}
+
+function sendNotifications() {
+    alert('Sugerimos que descanse 10 minutos agora');
+    new Notification('Sugerimos que descanse 10 minutos agora');
+}
+
 function decreaseSecond() {
-    if (labelMinutes.textContent >= 0 && labelSeconds.textContent > 0) {
+    if (labelMinutes.textContent >= 0 && labelSeconds.textContent >= 0) {
         if (labelSeconds.textContent > 0) {
             labelSeconds.innerHTML = labelSeconds.textContent - 1;
-        } else {
+        } else if (labelMinutes.textContent > 0) {
             setLabelMinutesAndLabelSeconds();
-        }
-    } else {
-        if (count < 3) {
-            count++;
         } else {
-            alert('ora do intervalo');
-            count = 0;
+            if (count < 3 && activeInterval == false) {
+                count++;
+            } else if (count == 3 && activeInterval == false) {
+                sendNotifications();
+                count = 0;
+            }
+
+            if (activeInterval == false) {
+                amountOfPomodoros++;
+                setAmountOfPomodoros(amountOfPomodoros);
+            }
+            activeInterval = false;
+            setCount(count);
+            showButtons();
+            setLabelAmountOfPomodoros(amountOfPomodoros);
+            alarm.play();
+            clearInterval(interval);
         }
-        setCount(count);
-        button.style.display = 'block';
-        clearInterval(interval);
     }
 }
